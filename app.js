5d852a82fb64a062c78b64f3656da2ce80f425b4
Balefather <balefather@gmail.com>
2017-10-25 10:23:28 +0200

    Indented that mofo
    yeah, i did

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 <<<<<<< HEAD
 
 (function(){
-var app = angular.module('absenceList', []);
+    var app = angular.module('absenceList', []);
 
-app.controller('StudentsController', function($scope) {
-  $scope.studentList = students;
+    app.controller('StudentsController', function($scope) {
+        $scope.studentList = students;
 
-});
+    });
 
-var students =[
-{ name: 'Jens', absence: true},
-{ name: 'Henrik', absence: false},
-{ name: 'Peter',absence: false}
+    var students =[
+        { name: 'Jens', absence: true},
+        { name: 'Henrik', absence: false},
+        { name: 'Peter',absence: false}
 
-];
+    ];
 
 })();
 =======
