29873f1547f22d266590a4a22defa89e0148df30
jona1411 <jonathan.klemensen@gmail.com>
2017-10-25 09:45:42 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+
+(function(){
+var app = angular.module('absenceList', []);
+
+app.controller('StudentsController', function($scope) {
+  $scope.studentList = students;
+
+});
+
+var students =[
+{ name: 'Jens', absence: true},
+{ name: 'Henrik', absence: false},
+{ name: 'Peter',absence: false}
+
+];
+
+})();
